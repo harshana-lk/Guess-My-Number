893c4b1a089b9dd00a6b173a1dea43e5941407ab
harshana-lk <harshanajayaweerayt@gmail.com>
2023-03-11 00:48:01 +0530

    Handling Click Events

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.score').textContent = 13;
-document.querySelector('.number').textContent = 10;
+// document.querySelector('.score').textContent = 13;
+// document.querySelector('.number').textContent = 10;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const nermo = Number(
+    (document.querySelector('.guess').value = Math.trunc(
+      Math.random() * 20 + 1
+    ))
+  );
+  if (nermo !== 10) {
+    document.querySelector('.message').textContent = '😒 Wrong Number !';
+  } else if (nermo == 10) {
+    document.querySelector('.message').textContent = '🎉 Correct Number !';
+  }
+});
