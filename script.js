08d1b7855730bb7d825eaa0bca882dc7c4c62cd1
harshana-lk <harshanajayaweerayt@gmail.com>
2023-03-10 23:01:08 +0530

    Selecting and Manipulating Elements

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.score').textContent = 13;
+document.querySelector('.number').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
