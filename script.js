083cfdd313bde520bda492669b8a1187b0e003f6
harshana-lk <harshanajayaweerayt@gmail.com>
2023-03-12 21:28:38 +0530

    Minipulating CSS Styles

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ document.querySelector('.check').addEventListener('click', function () {
     //  when palyer wins the game
   } else if (guessNumber === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number !';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
 
     // when player inputs a number that too high
   } else if (guessNumber > secretNumber) {
